b9eef3072f5478ddc52a78756bc9d0869828217a
innotimur <daggerguard@gmail.com>
2021-03-23 22:37:44 +0300

    layouts in progress

---
+++ src/assets/index.tsx
@@ -0,0 +1,7 @@
+import Logo from './icons/logo.png';
+import Google from './icons/google.png';
+
+export {
+    Logo,
+    Google,
+}
--- src/containers/login/index.tsx
+++ src/containers/login/index.tsx
@@ -1,13 +1,46 @@
 import React from 'react';
 
+import { Logo, Google } from '../../assets';
 import style from './style.css';
 
 // import './style.css';
 
 const Login = () => (
     <div className={style.wrapper}>
-        Будущая страница логина corethings
+        <div>
+            <img className={style.logo} src={Logo}/> 
+            <div className={style.appName}>
+                Corethings
+            </div>
+        </div>
+        <div className={style.mainBlock}>
+            <div>
+                Вход в систему
+            </div>
+            <div>
+            <input className={style.inputField} name="Login" placeholder="Введите логин"></input>
+            </div>
+            <div>
+            <input className={style.inputField} name="Password" placeholder="Введите пароль"></input>
+            </div>
+            <button className={style.entryButton}>
+                Войти
+            </button>
+            <div>
+                Или
+            </div>
+            <button className={style.entryButton}>
+                <img className={style.googleLogo} src={Google}/>
+                Войти с помощью Google
+                </button>
+            <div>
+                Забыли пароль?
+            </div>
+            <div>
+                Зарегистрироваться
+            </div>
+        </div>
     </div>
-);
+)
 
 export default Login
